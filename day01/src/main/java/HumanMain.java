c2fd7ddeae0b9ae31cbffe7a674b6fe5e7940866
Kolos1974 <karlovitz.kolos@gmail.com>
2021-10-20 23:47:22 +0200

    Human class task done

---
+++ day01/src/main/java/HumanMain.java
@@ -0,0 +1,16 @@
+public class HumanMain {
+
+    public static void main(String[] args) {
+        Human human = new Human("Béla", 34);
+
+        Human humanother = new Human("Géza", 56);
+
+        System.out.println(human.getName());
+        System.out.println(human.getAge());
+
+        System.out.println(humanother.getName());
+        System.out.println(humanother.getAge());
+
+
+    }
+}
