01d940908210171406542ca40ff21c702f9498ef
Kolos1974 <karlovitz.kolos@gmail.com>
2021-10-21 00:11:14 +0200

    Geometry task done

---
+++ day02/src/main/java/Geometry.java
@@ -0,0 +1,9 @@
+public class Geometry {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(15.6, 60.2);
+
+        System.out.println(rectangle.calculateArea());
+
+    }
+}
---
+++ day02/src/main/java/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    double sideA;
+    double sideB;
+
+    public Rectangle(double sideA, double sideB){
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public double calculateArea(){
+        return sideA*sideB;
+    }
+}
